6e70378b9a89259fdfeebc46054c35202bb162b3
Nayem khan <nayem178548@gmail.com>
2025-12-11 23:31:54 +0000

    headers and meals are added

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logoImg from '../assets/logo.jpg'
+export default function Header(){
+
+return(
+<header id="main-header">
+<div id="title">
+    <img src={logoImg}/>
+    <h1>React food </h1>
+</div>
+<nav>
+<button>Cart(0)</button>
+</nav>
+
+
+</header>
+);
+
+}
---
+++ src/components/meals.jsx
@@ -0,0 +1,17 @@
+export default function Meals(){
+
+    async function fetchMeals(){
+
+constfetch('http://localhost:3000/meals');
+        
+    }
+
+
+
+return <ul id="meals">
+
+
+
+</ul>
+
+}
