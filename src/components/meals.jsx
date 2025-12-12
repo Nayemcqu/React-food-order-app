4b4ed13044bda8ced6d0f684f4fff3e10b085849
Nayem khan <nayem178548@gmail.com>
2025-12-12 00:00:09 +0000

    meals done

--- src/components/meals.jsx
+++ src/components/meals.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
+
+
 export default function Meals(){
 
-    async function fetchMeals(){
+const[loadedMeals,setLoadedMeals]=useState([]);
+useEffect(()=>{
+async function fetchMeals(){
 
-constfetch('http://localhost:3000/meals');
-        
-    }
+const response=await fetch('http://localhost:3000/meals');
+
+if(!response.ok){
+    //
+}
+const meals=await response.json();        
+setLoadedMeals(meals);
+}
+fetchMeals();
+},[]);
 
 
 
-return <ul id="meals">
 
+
+
+return (<ul id="meals">
+
+{
+
+loadedMeals.map(meals=><li key={meals.id}>
+{meals.name}
+
+</li>)
+
+}
 
 
 </ul>
-
+);
 }
